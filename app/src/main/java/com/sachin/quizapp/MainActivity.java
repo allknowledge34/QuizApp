55067376f9e5ab22cb6b1559d99751cf4273a786
sachin kumar <allknowledge34@gmail.com>
2025-04-14 15:48:16 +0530

    second change

--- app/src/main/java/com/sachin/quizapp/MainActivity.java
+++ app/src/main/java/com/sachin/quizapp/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.main);
@@ -38,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 if (drawerLayout.isDrawerOpen(GravityCompat.START)){
                     drawerLayout.closeDrawer(GravityCompat.START);
                 }else {
