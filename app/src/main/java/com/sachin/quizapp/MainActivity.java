c5279c693f10dd319a65b18bede565d3b5879b04
sachin kumar <allknowledge34@gmail.com>
2025-04-14 14:06:36 +0530

    Initial commit

---
+++ app/src/main/java/com/sachin/quizapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.sachin.quizapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menu;
+    View header;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        menu = findViewById(R.id.menu);
+
+        header = navigationView.getHeaderView(0);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.share){
+                    String shareBody = "Hye, I am Using best online quiz app"+"http://play.google.com/store/apps/details?id="+MainActivity.this.getPackageName();
+                }
+                return false;
+            }
+        });
+    }
+}
